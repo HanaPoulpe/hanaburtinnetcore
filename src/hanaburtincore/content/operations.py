9df2ae0bfbf24550b50e1d772c141be2643e3aa5
HanaPoulpe <hana@hanapoulpe.org>
2023-08-06 15:51:19 +0200

    feat: :sparkles: Add article operrations

---
+++ src/hanaburtincore/content/operations.py
@@ -0,0 +1,167 @@
+import datetime
+
+from django.contrib.auth import models as auth_models
+from django.db import transaction
+
+from hanaburtincore.content import models as content_models
+from hanaburtincore.utils import localtime, operations
+
+
+class DuplicateArticleError(operations.OperationError):
+    pass
+
+
+def create_article(
+    *,
+    name: str,
+    content_format: content_models.TextFormats,
+    content: str,
+    user: auth_models.User | None,
+) -> content_models.Article:
+    """
+    Create a new article in the database.
+
+    :param name: Article title
+    :param content_format: Article text format
+    :param content: Article content
+    :param user: User creating the article
+    :return: the new article
+    :raise operations.OperationError: Impossible to create an article with those data.
+    """
+    try:
+        with transaction.atomic():
+            return content_models.Article.objects.create(
+                name=name,
+                format=content_format,
+                content=content,
+                created_by=user,
+                updated_by=user,
+            )
+    except Exception as err:
+        if content_models.Article.objects.filter(name=name).exists():
+            raise DuplicateArticleError(
+                operation_name="create_article",
+                message="Article %(name)s already exists." % {"name": name},
+            ) from err
+        raise operations.OperationError(
+            "create_article",
+            message="Failed to create article.",
+        ) from err
+
+
+@operations.operation(atomic=True)
+def update_article(
+    *,
+    user: auth_models.User | None,
+    article: content_models.Article,
+    name: str | None = None,
+    content_format: content_models.TextFormats | None = None,
+    content: str | None = None,
+) -> content_models.Article:
+    """
+    Update the article match.
+
+    :param user: User updating the article
+    :param article: Existing article object
+    :param name: new article title
+    :param content_format: new text format, if set, new content must be provided
+    :param content: new article content
+    :return: updated article
+    """
+    if content_format and not content:
+        raise ValueError("Article format can't be changed without changing its content.")
+
+    article.updated_by = user
+    if name:
+        article.name = name
+    if content_format:
+        article.format = content_format
+    if content:
+        article.content = content
+
+    try:
+        article.save()
+    except Exception as err:
+        raise operations.OperationError(
+            operation_name="update_article",
+            message="Failsed to update article.",
+        ) from err
+    return article
+
+
+@operations.operation(atomic=True)
+def publish_article(
+    *,
+    article: content_models.Article,
+    user: auth_models.User | None,
+    publication_date: datetime.datetime | None = None,
+) -> content_models.Article:
+    """
+    Publish a new article.
+
+    :param article: Article to publish
+    :param user: User publishing the article
+    :param publication_date: Publication date or None for now
+    :return: published article
+    :raise operations.OperationError: Operation is not possible.
+    """
+    if article.is_published:
+        raise operations.OperationError(
+            operation_name="publish_article",
+            message="Article is already published.",
+        )
+    if article.is_redacted:
+        raise operations.OperationError(
+            operation_name="publish_article",
+            message="Article have been redacted.",
+        )
+
+    article.updated_by = user
+    article.published_at = publication_date or localtime.now()
+
+    try:
+        article.save()
+    except Exception as err:
+        raise operations.OperationError(
+            operation_name="publish_article",
+            message="Failed to publish article.",
+        ) from err
+
+    return article
+
+
+@operations.operation(atomic=True)
+def redact_article(
+    *,
+    article: content_models.Article,
+    user: auth_models.User | None,
+    redaction_date: datetime.datetime | None = None,
+) -> content_models.Article:
+    """
+    Redact an article.
+
+    :param article: article to redact
+    :param user: User redacting the article
+    :param redaction_date: Effective redaction date, None for now
+    :return: redacted article
+    :raise operations.OperationError: Operation is not possible
+    :raise ValueError: Parameters are not valid.
+    """
+    if not article.is_published:
+        raise operations.OperationError(
+            operation_name="redact_article",
+            message="Cannot redact an unpublished article.",
+        )
+    if article.is_redacted:
+        raise operations.OperationError(
+            operation_name="redact_article",
+            message="Article is already redacted.",
+        )
+    if redaction_date and article.published_at > redaction_date:
+        raise ValueError("Article cannot be redacted before its publication.")
+
+    article.updated_by = user
+    article.redacted_at = redaction_date or localtime.now()
+    article.save()
+
+    return article
---
+++ src/tests/integration/content/test_operations.py
@@ -0,0 +1,185 @@
+from typing import Any
+
+import lorem
+import pytest
+import time_machine
+
+from hanaburtincore.content import models
+from hanaburtincore.content import operations as content_operations
+from hanaburtincore.utils import localtime, operations
+
+
+class TestArticleOperations:
+    @pytest.fixture(autouse=True)
+    def set_time(self, time_machine: time_machine.TimeMachineFixture) -> None:
+        time_machine.move_to("2023-08-05 00:00:00", tick=False)
+
+    @pytest.fixture
+    def title(self) -> str:
+        return lorem.get_sentence()
+
+    @pytest.fixture
+    def content(self) -> str:
+        return lorem.get_paragraph()
+
+    @pytest.fixture
+    def published(
+        self, request: Any, published_article: models.Article, redacted_article: models.Article
+    ) -> models.Article:
+        if request.param == "published":
+            return published_article
+        return redacted_article
+
+    @pytest.fixture
+    def not_published(
+        self, request: Any, article: models.Article, redacted_article: models.Article
+    ) -> models.Article:
+        if request.param == "redacted":
+            return redacted_article
+        return article
+
+    @pytest.mark.django_db
+    def test_create_article(
+        self,
+        title: str,
+        content: str,
+    ) -> None:
+        # TODO: Add user fixture
+        article = content_operations.create_article(
+            name=title,
+            content_format=models.TextFormats.PLAIN_TEXT,
+            content=content,
+            user=None,
+        )
+
+        assert article.name == title
+        assert article.content == content
+        assert article.created_at == localtime.now()
+        assert article.updated_at == localtime.now()
+
+    @pytest.mark.django_db
+    def test_create_duplicate_article(
+        self,
+        article: models.Article,
+        content: str,
+    ) -> None:
+        with pytest.raises(content_operations.DuplicateArticleError):
+            content_operations.create_article(
+                name=article.name,
+                content=content,
+                content_format=models.TextFormats.PLAIN_TEXT,
+                user=None,
+            )
+
+    @pytest.mark.django_db
+    def test_update_article(
+        self,
+        content: str,
+        article: models.Article,
+        time_machine: time_machine.TimeMachineFixture,
+    ) -> None:
+        # TODO: Add user fixture
+        original_created_at = article.created_at
+        original_created_by = article.created_by
+
+        time_machine.move_to("2023-08-06 00:00:00", tick=False)
+
+        article = content_operations.update_article(
+            user=None,
+            article=article,
+            content=content,
+        )
+
+        assert article.content == content
+        assert original_created_by == article.created_by
+        assert original_created_at == article.created_at
+        assert article.updated_at == localtime.now()
+
+    @pytest.mark.django_db
+    @pytest.mark.parametrize(
+        "content_format",
+        [models.TextFormats.PLAIN_TEXT, models.TextFormats.HTML, models.TextFormats.MARKDOWN],
+    )
+    def test_update_article_format(
+        self,
+        article: models.Article,
+        content: str,
+        content_format: models.TextFormats,
+    ) -> None:
+        article = content_operations.update_article(
+            user=None,
+            article=article,
+            content_format=content_format,
+            content=content,
+        )
+
+        assert article.content == content
+        assert article.format == content_format
+
+    @pytest.mark.django_db
+    @pytest.mark.parametrize(
+        "content_format",
+        [models.TextFormats.PLAIN_TEXT, models.TextFormats.HTML, models.TextFormats.MARKDOWN],
+    )
+    def test_update_article_format_without_content(
+        self,
+        article: models.Article,
+        content_format: models.TextFormats,
+    ) -> None:
+        with pytest.raises(ValueError):
+            content_operations.update_article(
+                user=None,
+                article=article,
+                content_format=content_format,
+            )
+
+    @pytest.mark.django_db
+    def test_publish_article(self, article: models.Article) -> None:
+        article = content_operations.publish_article(
+            user=None,
+            article=article,
+        )
+
+        assert article.is_published
+
+    @pytest.mark.django_db
+    @pytest.mark.parametrize(
+        "published",
+        ("published", "redacted"),
+        indirect=True,
+    )
+    def test_publish_published_article(self, published: models.Article) -> None:
+        with pytest.raises(operations.OperationError):
+            content_operations.publish_article(
+                user=None,
+                article=published,
+            )
+
+    @pytest.mark.django_db
+    def test_redact_article(self, published_article: models.Article) -> None:
+        article = content_operations.redact_article(
+            user=None,
+            article=published_article,
+        )
+
+        assert not article.is_published
+        assert article.is_redacted
+
+    @pytest.mark.django_db
+    @pytest.mark.parametrize("not_published", ("not_published", "redacted"), indirect=True)
+    def test_redact_not_redactable_article(self, not_published: models.Article) -> None:
+        with pytest.raises(operations.OperationError):
+            content_operations.redact_article(
+                user=None,
+                article=not_published,
+            )
+
+    @pytest.mark.django_db
+    def test_redact_article_with_invalid_dates(self, published_article: models.Article) -> None:
+        long_past = localtime.make_aware(localtime.datetime(1970, 1, 1))
+        with pytest.raises(ValueError):
+            content_operations.redact_article(
+                user=None,
+                article=published_article,
+                redaction_date=long_past,
+            )
