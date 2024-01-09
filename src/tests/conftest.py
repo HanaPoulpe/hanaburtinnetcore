abdc4a03117059ce9b19d2fb52d252c61b5e4d70
HanaPoulpe <hana@hanapoulpe.org>
2024-01-09 20:01:17 +0100

    docs: :books: fix conftest.py typing

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -4,5 +4,5 @@ import pytest
 from tests.fixtures import *
 
 
-def pytest_runtest_setup(item):  # type: ignore
+def pytest_runtest_setup(item: pytest.Item) -> None:
     configurations.wsgi.get_wsgi_application()
