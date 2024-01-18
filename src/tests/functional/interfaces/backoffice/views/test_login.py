8699c71425bba19992760e230cc34aa2a8084a2c
HanaPoulpe <hana@hanapoulpe.org>
2024-01-18 21:20:31 +0100

    test: :heavy_check_mark: Add tests for login

---
+++ src/tests/functional/interfaces/backoffice/views/test_login.py
@@ -0,0 +1,70 @@
+import attr
+import pytest
+from django import test as django_test
+from django.contrib.auth import models as auth_models
+from faker import Faker
+
+from queenbees.interfaces.backoffice.views import _login as login_view
+from tests.factories import users as users_factories
+
+faker = Faker()
+
+
+@attr.define
+class RejectedLogin:
+    username: str
+    password: str
+
+
+class TestPasswordLogin:
+    @pytest.fixture
+    def rejected_logins(self, request: pytest.FixtureRequest) -> RejectedLogin:
+        if request.param == "invalid_password":
+            user = users_factories.User()
+            user_password = faker.password()
+            user.set_password(user_password)
+
+            while True:
+                wrong_password = faker.password()
+                if wrong_password != user_password:
+                    return RejectedLogin(
+                        user.username,
+                        wrong_password,
+                    )
+
+        if request.param == "password_login_not_allowed":
+            user = users_factories.User()
+            user.set_unusable_password()
+            return RejectedLogin(
+                user.username,
+                faker.password(),
+            )
+
+        if request.param == "invalid_user":
+            return RejectedLogin(
+                faker.user_name(),
+                faker.password(),
+            )
+
+        raise Exception(f"Invalid test parameter: {request.param}")
+
+    @pytest.mark.django_db
+    def test_login_successful(
+        self, client: django_test.Client, user_with_password: tuple[auth_models.User, str]
+    ) -> None:
+        user, password = user_with_password
+
+        assert client.login(username=user, password=password)
+
+    @pytest.mark.parametrize(
+        "rejected_logins",
+        ["invalid_password", "password_login_not_allowed", "invalid_user"],
+        indirect=True,
+    )
+    @pytest.mark.django_db
+    def test_login_failed(
+        self, client: django_test.Client, rejected_logins: RejectedLogin
+    ) -> None:
+        assert not client.login(
+            username=rejected_logins.username, password=rejected_logins.password
+        )
