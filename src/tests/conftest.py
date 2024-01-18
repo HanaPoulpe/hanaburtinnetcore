bc222653b16278f186485562eb4eba56b5adfa1e
HanaPoulpe <hana@hanapoulpe.org>
2024-01-18 19:09:11 +0100

    test: :heavy_check_mark: fix test config

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,9 +1,12 @@
-import sys
+import os.path
 
+import configurations.wsgi
+import dotenv
 import pytest
 
 from tests.fixtures import *
 
 
 def pytest_runtest_setup(item: pytest.Item) -> None:
+    dotenv.load_dotenv(os.path.join(os.path.dirname(__file__), ".env"))
     from configurations.management import execute_from_command_line
