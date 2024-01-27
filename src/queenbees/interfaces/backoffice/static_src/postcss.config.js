b38bc1d5843b46f9ae5b3762c75ecd8cdf032075
HanaPoulpe <hana@hanapoulpe.org>
2024-01-27 08:21:55 +0100

    fix: :bug: correct tailwind nesting configuration

--- src/queenbees/interfaces/backoffice/static_src/postcss.config.js
+++ src/queenbees/interfaces/backoffice/static_src/postcss.config.js
@@ -4,7 +4,7 @@ const postcssPresetEnv = require("postcss-preset-env");
 module.exports = {
   plugins: {
     'postcss-import': {},
-    'tailwindcss/nesting': 'postcss-nesting',
+    'tailwindcss/nesting': {},
     tailwindcss: {},
     autoprefixer: {},
     'postcss-preset-env': {
