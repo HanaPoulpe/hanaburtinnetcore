349d35a19753473db99fbd2acbf03d10d995a73f
HanaPoulpe <hana@hanapoulpe.org>
2024-02-03 06:44:46 +0100

    feat: :sparkles: Add article details view

--- src/queenbees/core/content/models.py
+++ src/queenbees/core/content/models.py
@@ -20,7 +20,7 @@ class FileSourceTypes(models.TextChoices):
 
 # Models #
 class GenericContent(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     name = models.CharField(max_length=100, blank=False, null=False, unique=True)
 
     created_at = models.DateTimeField(
--- src/queenbees/interfaces/backoffice/content/urls.py
+++ src/queenbees/interfaces/backoffice/content/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     urls.path("articles/", views.ArticleList.as_view(), name="article-list"),
+    urls.path("article/<uuid:id>/", views.ArticleView.as_view(), name="article-view"),
 ]
